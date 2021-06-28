6c861d825f441467ab64be1eb1e6b4c43634a45d
sergeykin <sergeykinn@mail.ru>
2021-06-28 19:06:37 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/ru/job4j/job4j_car_accident/SecurityInit.java
@@ -0,0 +1,6 @@
+package ru.job4j.job4j_car_accident;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
